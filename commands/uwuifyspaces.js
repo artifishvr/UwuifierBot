a6b095948353e0e58616bf4bbb6e0182bfb84f0d
ArtificialButter <59352535+artificialbutter@users.noreply.github.com>
2022-05-02 22:17:47 -0700

    Add more options

---
+++ commands/uwuifyspaces.js
@@ -0,0 +1,39 @@
+const { SlashCommand, CommandOptionType } = require('slash-create');
+const { Uwuifier } = require('@patarapolw/uwuifier');
+
+module.exports = class extends SlashCommand {
+    constructor(creator) {
+        super(creator, {
+            name: 'uwuifyspaces',
+            description: 'Just uwuifies spaces (adds random s-s-stutters, actions, and faces in between words)',
+            options: [
+                {
+                    name: 'text',
+                    type: CommandOptionType.STRING,
+                    description: 'Text to Uwuify',
+                    required: true
+                }
+            ],
+
+            guildIDs: process.env.DISCORD_GUILD_ID ? [ process.env.DISCORD_GUILD_ID ] : undefined
+        });
+    }
+
+    async run (ctx) {
+    try {
+        const uwuifier = new Uwuifier();
+        const text = ctx.options.text;
+
+        await ctx.defer();
+         
+        // send to statcord
+        const { statcord } = require('..');
+        statcord.postCommand("Uwuify", ctx.user.id);
+
+
+
+        ctx.sendFollowUp({ content: uwuifier.uwuifySpaces(text) });
+    } catch (error) {
+        console.error(error);
+    }}
+};
---
+++ commands/uwuifywords.js
@@ -0,0 +1,39 @@
+const { SlashCommand, CommandOptionType } = require('slash-create');
+const { Uwuifier } = require('@patarapolw/uwuifier');
+
+module.exports = class extends SlashCommand {
+    constructor(creator) {
+        super(creator, {
+            name: 'uwuifywords',
+            description: 'Just uwuifies words (turns something like hello world into hewwo worwld)',
+            options: [
+                {
+                    name: 'text',
+                    type: CommandOptionType.STRING,
+                    description: 'Text to Uwuify',
+                    required: true
+                }
+            ],
+
+            guildIDs: process.env.DISCORD_GUILD_ID ? [ process.env.DISCORD_GUILD_ID ] : undefined
+        });
+    }
+
+    async run (ctx) {
+    try {
+        const uwuifier = new Uwuifier();
+        const text = ctx.options.text;
+
+        await ctx.defer();
+         
+        // send to statcord
+        const { statcord } = require('..');
+        statcord.postCommand("Uwuify", ctx.user.id);
+
+
+
+        ctx.sendFollowUp({ content: uwuifier.uwuifyWords(text) });
+    } catch (error) {
+        console.error(error);
+    }}
+};
