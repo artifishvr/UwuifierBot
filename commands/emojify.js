9fcbb51a824a3af4de8b7ea6aa7d33c94eb7c844
ArtificialButter <59352535+artificialbutter@users.noreply.github.com>
2022-05-08 15:27:40 -0700

    Add emojify density option

--- commands/emojify.js
+++ commands/emojify.js
@@ -12,6 +12,12 @@ module.exports = class extends SlashCommand {
                     type: CommandOptionType.STRING,
                     description: 'Text to Emojify',
                     required: true
+                },
+                {
+                    name: 'density',
+                    type: CommandOptionType.STRING,
+                    description: 'The density of the emojis. Can be 1-100 (default: 100)',
+                    required: false
                 }
             ],
 
@@ -22,7 +28,18 @@ module.exports = class extends SlashCommand {
     async run (ctx) {
     try {
         const text = ctx.options.text;
-        const emojifiedtext = replaceWord.emojipasta(text);
+        var emojifiedtext = "";
+
+        if (ctx.options.density) {
+            if (ctx.options.density >= 100) { // if density is too high
+            ctx.sendFollowUp({ content: "Density can only be from 1-100" }); // send error message & exit
+            return; 
+            } else {
+            emojifiedtext = replaceWord.emojipasta(text, ctx.options.density); // emojify text with requested density
+            }
+        } else {
+            emojifiedtext = replaceWord.emojipasta(text, 100); // emojify text with default density if none is specified
+        }
 
         await ctx.defer();
          
@@ -30,10 +47,10 @@ module.exports = class extends SlashCommand {
         const { statcord } = require('..');
         statcord.postCommand("Emojify", ctx.user.id);
 
-        if (emojifiedtext.length <= 2000) {
-          ctx.sendFollowUp({ content: emojifiedtext });
+        if (emojifiedtext.length <= 2000) { // if emojified text is too long to send in discord
+          ctx.sendFollowUp({ content: emojifiedtext }); // send emojified text if it isn't too long
         } else {
-          ctx.sendFollowUp({ content: "That text was too long to emojify." });
+          ctx.sendFollowUp({ content: "That text was too long to emojify." }); // send error message if it is too long
         }
     } catch (error) {
         console.error(error);
